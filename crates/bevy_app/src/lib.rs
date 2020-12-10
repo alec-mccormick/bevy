366a085448945b4fa71b2a5cfbe46d5e03cd4b3f
alec-mccormick <alec.s.mccormick@gmail.com>
2020-12-09 17:05:36 -0800

    event stage added

--- crates/bevy_app/src/lib.rs
+++ crates/bevy_app/src/lib.rs
@@ -6,14 +6,18 @@ pub mod startup_stage;
 mod app;
 mod app_builder;
 mod event;
+mod event_stage;
 mod plugin;
 mod plugin_group;
 mod schedule_runner;
+mod any_event_stage;
 
 pub use app::*;
 pub use app_builder::*;
 pub use bevy_derive::DynamicPlugin;
 pub use event::*;
+pub use event_stage::*;
+pub use any_event_stage::*;
 pub use plugin::*;
 pub use plugin_group::*;
 pub use schedule_runner::*;
@@ -23,6 +27,7 @@ pub mod prelude {
         app::App,
         app_builder::AppBuilder,
         event::{EventReader, Events},
+        event_stage::EventStage,
         stage, DynamicPlugin, Plugin, PluginGroup,
     };
 }
